a4b3bfa02da24739169dc706751966ff40cc3e82
mrinmoysana7 <mrinmoysana7@gmail.com>
2026-04-15 21:07:10 +0530

    Create folder structure

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <h2 className="font-bold text-[48px] text-white"> Human Ledger</h2>
+  </StrictMode>,
+)
